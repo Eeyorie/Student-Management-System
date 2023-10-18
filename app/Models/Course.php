0ec1bde5b7c2dc9c759603c9cbaa3662f4da1fd2
Eeyorie <vava.frncssena@gmail.com>
2023-10-18 20:53:02 +0800

    Course Model, Controller, Tables and Blades

---
+++ app/Models/Course.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'course_name',
+        'course_code',
+        'description',
+        'credits',
+        'instructor_id',
+        'semester',
+        'max_enrollment',
+        'start_date',
+        'end_date',
+    ];
+
+    public function instructor()
+    {
+        return $this->belongsTo(Faculty::class, 'instructor_id');
+    }
+}
